5ca915e14bf16f125450ab98fe919f5af457ce48
DonBatman <don@serfdon.com>
2016-03-24 12:44:30 -0700

    Added the rest of the colors

--- init.lua
+++ init.lua
@@ -3,6 +3,18 @@ myfences.colors = {
 	{"red", "Red", "842800ae"},
 	{"green", "Green", "0c4916b3"},
 	{"white", "White", "ffffffb3"},
+	{"black","Black","000000"},
+	{"blue","Blue","0B0B3B"},
+	{"brown","Brown","190B07"},
+	{"cyan","Cyan","00a4e8"},
+	{"darkgreen","Dark Green","071907"},
+	{"darkgrey","Dark Grey","1C1C1C"},
+	{"grey","Grey","848484"},
+	{"magenta","Magenta","e8008e"},
+	{"orange","Orange","e8b400"},
+	{"pink","Pink","FE2E9A"},
+	{"violet","Violet","08088A"},
+	{"yellow","Yellow","f4dd1b"},
 }
 
 dofile(minetest.get_modpath("myfences").."/picket.lua")
