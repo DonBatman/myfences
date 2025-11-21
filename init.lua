74cebdbccf483c31efcab2c3af8ea5e29753b458
Don <serfdon@gmail.com>
2025-11-21 07:02:14 -0800

    Add files via upload

--- init.lua
+++ init.lua
@@ -17,6 +17,42 @@ myfences.colors = {
 	{"yellow",     "Yellow",     "e3ff00"},
 }
 
+if core.get_modpath("mydye") then
+	myfences.colors = {
+	{"black",      	"Black",      		"000000"},
+	{"blue",       	"Blue",       		"2000c9"},
+	{"brown",     	"Brown",      		"954c05"},
+	{"cyan",      	"Cyan",       		"01ffd8"},
+	{"darkgreen", 	"Dark Green",  		"005b07"},
+	{"darkgrey",  	"Dark Grey",   		"303030"},
+	{"green",     	"Green",      		"61ff01"},
+	{"grey",       	"Grey",       		"5b5b5b"},
+	{"magenta",    	"Magenta",    		"ff05bb"},
+	{"orange",     	"Orange",     		"ff8401"},
+	{"pink",      	"Pink",       		"ff65b5"},
+	{"red",        	"Red",        		"ff0000"},
+	{"violet",     	"Violet",     		"ab23b0"},
+	{"white",      	"White",      		"ffffff"},
+	{"yellow",     	"Yellow",     		"e3ff00"},
+	{"peachpuff",	"Peachpuff", 		"FFDAB9"},
+	{"navy",		"Navy", 			"000080"},
+	{"coral",		"Coral", 			"FF7F50"},
+	{"khaki",		"Khaki", 			"F0E68C"},
+	{"lime",		"Lime", 			"00FF00"},
+	{"light_pink",	"Light Pink", 		"FFB6C1"},
+	{"light_grey",	"Light Grey", 		"D3D3D3"},
+	{"purple",		"Purple", 			"800080"},
+	{"maroon",		"Maroon", 			"800000"},
+	{"aquamarine",	"Aqua Marine", 		"7FFFD4"},
+	{"chocolate",	"Chocolate", 		"D2691E"},
+	{"crimson",		"Crimson", 			"DC143C"},
+	{"olive",		"Olive", 			"808000"},
+	{"white_smoke",	"White Smoke", 		"F5F5F5"},
+	{"mistyrose",	"Misty Rose", 		"FFE4E1"},
+	{"orchid",		"Orchid", 			"DA70D6"},
+	}
+end
+
 dofile(minetest.get_modpath("myfences").."/picket.lua")
 dofile(minetest.get_modpath("myfences").."/privacy.lua")
 dofile(minetest.get_modpath("myfences").."/garden.lua")
